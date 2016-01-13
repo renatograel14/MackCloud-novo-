a1dcbd1ba8a7b0bf396dab74564ef860d8ea2d82
Renato Teixeira <rteixeir@progress.com>
2016-01-13 17:03:42 +0000

    Changes made by Renato Teixeira (rteixeir@progress.com).

--- MackCloud/components/createArquivo/index.js
+++ MackCloud/components/createArquivo/index.js
@@ -1,21 +1,58 @@
 'use strict';
 
-app.createArquivo = kendo.observable({
-    onShow: function() {},
-    afterShow: function() {}
-});
-
 // START_CUSTOM_CODE_createArquivo
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
 
+app.createArquivo = kendo.observable({
+    image: null,
+    onShow: function (e) {
+        var dataProvider = app.data.mackCloud;
+
+        app.createArquivo.currentItem = e.view.params.aula;
+        dataProvider.Files.getById(e.view.params.imageId)
+            .then(function (data) {
+                    var file = data.result;
+
+                    app.createArquivo.image = file;
+                    $('#image').attr('src', file.Uri);
+
+                },
+                function (error) {
+                    alert(JSON.stringify(error));
+                });
+    },
+    afterShow: function () {
+        app.mobileApp.pane.loader.hide();
+    }
+});
+
 // END_CUSTOM_CODE_createArquivo
-(function(parent) {
+
+(function (parent) {
+    var dataProvider = app.data.mackCloud;
     var createArquivoModel = kendo.observable({
         fields: {
             titulo: '',
         },
-        submit: function() {},
-        cancel: function() {}
+        submit: function () {
+            var image = parent.image,
+                aulaId = parent.aula;
+
+            app.mobileApp.pane.loader.show();
+            dataProvider.data('Arquivo').create({
+                    'Titulo': createArquivoModel.fields.titulo,
+                    file: image,
+                    Tipo: "Imagem"
+                })
+                .then(function (data) {
+                        app.mobileApp.navigate('#components/dataListViewAulas/view.html');
+                    },
+                    function (error) {
+                        alert(JSON.stringify(error));
+                    });
+        },
+        cancel: function () {}
+
     });
 
     parent.set('createArquivoModel', createArquivoModel);
--- MackCloud/components/dataListViewAulas/index.js
+++ MackCloud/components/dataListViewAulas/index.js
@@ -1,8 +1,10 @@
 'use strict';
 
 app.dataListViewAulas = kendo.observable({
-    onShow: function() {},
-    afterShow: function() {}
+    onShow: function () {},
+    afterShow: function () {
+        app.mobileApp.pane.loader.hide();
+    }
 });
 
 // START_CUSTOM_CODE_dataListViewAulas
@@ -13,11 +15,11 @@ app.dataListViewAulas = kendo.observable({
 
 // START_CUSTOM_CODE_dataListViewAulasModel
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
-(function(parent) {
+(function (parent) {
     var dataProvider = app.data.mackCloud,
-        flattenLocationProperties = function(dataItem) {
+        flattenLocationProperties = function (dataItem) {
             var propName, propValue,
-                isLocation = function(value) {
+                isLocation = function (value) {
                     return propValue && typeof propValue === 'object' &&
                         propValue.longitude && propValue.latitude;
                 };
@@ -43,7 +45,7 @@ app.dataListViewAulas = kendo.observable({
                 field: 'Disciplina'
             },
 
-            change: function(e) {
+            change: function (e) {
                 var data = this.data();
                 for (var i = 0; i < data.length; i++) {
                     var dataItem = data[i];
@@ -76,7 +78,7 @@ app.dataListViewAulas = kendo.observable({
                 field: 'Tipo'
             },
 
-            change: function(e) {
+            change: function (e) {
                 var data = this.data();
                 for (var i = 0; i < data.length; i++) {
                     var dataItem = data[i];
@@ -107,32 +109,53 @@ app.dataListViewAulas = kendo.observable({
         dataSourceFile = new kendo.data.DataSource(dataSourceOptFile),
         dataListViewAulasModel = kendo.observable({
             dataSource: dataSource,
-            itemClick: function(e) {
-                dataSourceFile.fetch(function() {
+            itemClick: function (e) {
+                dataSourceFile.fetch(function () {
                     app.mobileApp.navigate('#components/dataListViewAulas/details.html?uid=' + e.dataItem.uid);
                 });
             },
-            addFile: function() {
-                var success = function(data) {
-                    navigator.notification.alert("Foi");
-                };
-                var error = function() {
-                    navigator.notification.alert("Unfortunately we could not add the image");
-                };
-                var config = {
-                    destinationType: Camera.DestinationType.DATA_URL,
-                    targetHeight: 400,
-                    targetWidth: 400
-                };
-                navigator.camera.getPicture(success, error, config);
-            },
-            detailsShow: function(e) {
+
+            detailsShow: function (e) {
                 var item = e.view.params.uid,
                     dataSource = dataListViewAulasModel.get('dataSource'),
                     itemModel = dataSource.getByUid(item);
 
                 itemModel.arquivos = dataSourceFile;
-                console.log(itemModel.arquivos);
+                itemModel.addFile = function () {
+                    var success = function (data) {
+
+                        var file = {
+                            "Filename": (Math.random() * 100000000000000000000000).toString(16) + ".png",
+                            "ContentType": "image/png",
+                            "CustomField": "",
+                            "base64": data
+                        };
+
+
+                        app.mobileApp.pane.loader.show();
+                        dataProvider.files.create(file,
+                            function (data) {
+                                app.mobileApp.navigate('#components/createArquivo/view.html?imageId=' + data.result.Id + "&aula=" + item);
+                            },
+                            function (error) {
+                                alert(JSON.stringify(error));
+                            });
+
+
+                    };
+                    var error = function () {
+                        navigator.notification.alert("Erro ao tirar a foto!");
+                        app.mobileApp.navigate('#components/dataListViewAulas/view.html');
+                    };
+                    var config = {
+                        destinationType: Camera.DestinationType.DATA_URL,
+                        targetHeight: 400,
+                        targetWidth: 400
+                    };
+                    navigator.camera.getPicture(success, error, config);
+                };
+
+
                 if (!itemModel.Disciplina) {
                     itemModel.Disciplina = String.fromCharCode(160);
                 }
